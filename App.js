097e78dc58a5222208a6e5d650a05f36a02b01d8
Matheus Neris <matheusneris@Matheuss-MacBook-Air.local>
2024-02-04 15:27:33 -0300

    melhorando as rotas

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -26,49 +26,7 @@ const HomeStack = () => (
   </Stack.Navigator>
 );
 
-const TabNavigator = () => (
-  <Tab.Navigator
-    screenOptions={({ route }) => ({
-      tabBarLabel: ({ focused, color }) => {
-        let labelStyle = { color: focused ? '#253494' : 'black' };
-        return <Text style={labelStyle}>{route.name}</Text>;
-      },
-      tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
-
-        switch (route.name) {
-          case 'Ínicio':
-            iconName = focused ? 'home' : 'home';
-            return <MaterialIcons name={iconName} size={size} color={color} />;
-          case 'Cadastro':
-            return null;
-          case 'Tarefas':
-            iconName = focused ? 'check' : 'check-outline';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-          case 'HelpGpt':
-            iconName = focused ? 'chat-question' : 'chat';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-          case 'Materias':
-            iconName = focused ? 'book-open' : 'book-outline';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-          case 'Menu':
-            iconName = focused ? 'clipboard-list' : 'clipboard-minus';
-            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
-          default:
-            return null;
-        }
-      },
-    })}
-  >
-    <Tab.Screen name="Ínicio" component={HomeStack} options={{ headerShown: false }} />
-    <Tab.Screen name="Tarefas" component={Home} options={{ headerShown: false }} />
-    <Tab.Screen name="HelpGpt" component={Home} options={{ headerShown: false }} />
-    <Tab.Screen name="Materias" component={Home} options={{ headerShown: false }} />
-    <Tab.Screen name="Menu" component={Menu} options={{ headerShown: false }} />
-  </Tab.Navigator>
-);
-
-export default function App() {
+const TabNavigator = ({ route }) => {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   useEffect(() => {
@@ -87,43 +45,104 @@ export default function App() {
   };
 
   return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarLabel: ({ focused, color }) => {
+          let labelStyle = { color: focused ? '#253494' : 'black' };
+          return <Text style={labelStyle}>{route.name}</Text>;
+        },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          switch (route.name) {
+            case 'Ínicio':
+              iconName = focused ? 'home' : 'home';
+              return <MaterialIcons name={iconName} size={size} color={color} />;
+            case 'Cadastro':
+              return null;
+            case 'Tarefas':
+              iconName = focused ? 'check' : 'check-outline';
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+            case 'HelpGpt':
+              iconName = focused ? 'chat-question' : 'chat';
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+            case 'Materias':
+              iconName = focused ? 'book-open' : 'book-outline';
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+            case 'Menu':
+              iconName = focused ? 'clipboard-list' : 'clipboard-minus';
+              return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+            default:
+              return null;
+          }
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Ínicio"
+        component={HomeStack}
+        options={{
+          headerShown: route.state && route.state.index > 0 ? false : true,
+          headerStyle: { backgroundColor: '#253494' },
+          headerTintColor: 'white',
+          title: 'Home',
+        }}
+      />
+      <Tab.Screen
+        name="Tarefas"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="HelpGpt"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Materias"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Tab.Screen
+        name="Menu"
+        component={Menu}
+        options={{
+          headerShown: route.state && route.state.index > 0 ? false : true,
+          headerStyle: { backgroundColor: '#253494' },
+          headerTintColor: 'white',
+          title: 'Menu',
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default function App() {
+  return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={isUserLoggedIn ? 'Home' : 'Login'}>
-        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen
-          name="Home"
-          component={TabNavigator}
-          options={{
-            headerShown: true,
-            headerStyle: { backgroundColor: '#253494' },
-            headerTintColor: 'white',
-            headerTitle: 'Home',
-            headerLeft: null,
-            gestureEnabled: false,
-          }}
-        />
-        <Stack.Screen
-          name="Cadastro"
-          component={Cadastro}
-          options={{
-            headerShown: true,
-            headerStyle: { backgroundColor: '#253494' },
-            headerTintColor: 'white',
-            headerTitle: 'Cadastro',
-          }}
-        />
+      <Stack.Navigator initialRouteName="Login" headerMode="none">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={TabNavigator} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen
           name="NewsScreen"
           component={NewsScreen}
-          options={{
+          options={({ navigation }) => ({
             headerShown: true,
             headerStyle: { backgroundColor: '#253494' },
             headerTintColor: 'white',
-            headerTitle: 'Notícias',
-          }}
+            title: 'Notícias',
+            headerLeft: () => (
+              <TouchableOpacity
+                style={{ marginLeft: 16 }}
+                onPress={() => navigation.goBack()}
+              >
+                <MaterialIcons name="arrow-back" size={24} color="white" />
+              </TouchableOpacity>
+            ),
+          })}
         />
-       
-      </Stack.Navigator>
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
