4d664a529dd05e47e65955b3e05cb17db19176f9
matheusnerisxavier <matheus.nerisxa@outlook.com>
2024-03-19 22:57:25 -0300

    ajustes

--- views/Stopwatches/stopwatches.js
+++ views/Stopwatches/stopwatches.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, Animated, Dimensions, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import { Ionicons } from '@expo/vector-icons';
 import config from '../../config/config.json';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { stopwatchesStyles } from './css/stopwatchesStyles';
 
 const StopwatchScreen = () => {
   const [time, setTime] = useState(0);
@@ -12,9 +13,15 @@ const StopwatchScreen = () => {
   const [selectedSubjectId, setSelectedSubjectId] = useState(null);
   const [subjects, setSubjects] = useState([]);
   const [userId, setUserId] = useState(null);
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const [width, setWidth] = useState(Dimensions.get('window').width);
 
   useEffect(() => {
     fetchUserData();
+    Dimensions.addEventListener('change', handleOrientationChange);
+    return () => {
+      Dimensions.removeEventListener('change', handleOrientationChange);
+    };
   }, []);
 
   useEffect(() => {
@@ -29,17 +36,31 @@ const StopwatchScreen = () => {
     return () => clearInterval(interval);
   }, [running]);
 
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
   const fetchUserData = async () => {
     try {
       const userDataString = await AsyncStorage.getItem('userData');
       if (userDataString) {
         const userData = JSON.parse(userDataString);
-        const { email } = userData;
-        const response = await fetch(`${config.urlRootNode}getUserId?email=${email}`);
-        const data = await response.json();
-        console.log('ID do usuário:', data.userId);
-        setUserId(data.userId);
-        fetchSubjects(data.userId);
+        const { email } = userData || {};
+        if (email) {
+          const response = await fetch(`${config.urlRootNode}getUserId?email=${email}`);
+          const data = await response.json();
+          console.log('ID do usuário:', data.userId);
+          setUserId(data.userId);
+          fetchSubjects(data.userId);
+        } else {
+          console.error('Email não encontrado nos dados do usuário');
+        }
+      } else {
+        console.error('Dados do usuário não encontrados');
       }
     } catch (error) {
       console.error('Erro ao buscar dados do usuário:', error);
@@ -96,43 +117,60 @@ const StopwatchScreen = () => {
       console.error('Erro ao salvar dados:', error);
     }
   };
-  
+
+  const handleHistory = () => {
+    console.log('Botão de histórico pressionado');
+  };
+
+  const handleOrientationChange = () => {
+    setWidth(Dimensions.get('window').width);
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.card}>
-        <Text style={styles.timerText}>{formatTime(time)}</Text>
-        <View style={styles.buttonsContainer}>
-          <TouchableOpacity style={styles.button} onPress={startStopwatch}>
-            <Ionicons name="play" size={24} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={pauseStopwatch}>
-            <Ionicons name="pause" size={24} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={resetStopwatch}>
-            <Ionicons name="refresh" size={24} color="white" />
-          </TouchableOpacity>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={stopwatchesStyles.container}>
+          <Animated.View style={[stopwatchesStyles.card, { opacity: fadeAnim }]}>
+            <Text style={stopwatchesStyles.timerText}>{formatTime(time)}</Text>
+            <View style={stopwatchesStyles.buttonsContainer}>
+              <TouchableOpacity style={stopwatchesStyles.button} onPress={startStopwatch}>
+                <Ionicons name="play" size={24} color="white" />
+              </TouchableOpacity>
+              <TouchableOpacity style={stopwatchesStyles.button} onPress={pauseStopwatch}>
+                <Ionicons name="pause" size={24} color="white" />
+              </TouchableOpacity>
+              <TouchableOpacity style={stopwatchesStyles.button} onPress={resetStopwatch}>
+                <Ionicons name="refresh" size={24} color="white" />
+              </TouchableOpacity>
+            </View>
+          </Animated.View>
+          <TextInput
+            style={[stopwatchesStyles.input, { width: width * 0.9 }]}
+            placeholder="Descrição"
+            value={description}
+            onChangeText={setDescription}
+          />
+          <Picker
+            selectedValue={selectedSubjectId}
+            onValueChange={(itemValue) => setSelectedSubjectId(itemValue)}
+            style={[stopwatchesStyles.picker, { width: width * 0.9 }]}
+          >
+            <Picker.Item label="Selecione uma matéria" value={null} />
+            {subjects.map((subject) => (
+              <Picker.Item key={subject.id} label={subject.name} value={subject.id} />
+            ))}
+          </Picker>
         </View>
-      </View>
-      <TextInput
-        style={styles.input}
-        placeholder="Descrição"
-        value={description}
-        onChangeText={setDescription}
-      />
-      <Picker
-        selectedValue={selectedSubjectId}
-        onValueChange={(itemValue) => setSelectedSubjectId(itemValue)}
-        style={styles.picker}
-      >
-        <Picker.Item label="Selecione uma matéria" value={null} />
-        {subjects.map((subject) => (
-          <Picker.Item key={subject.id} label={subject.name} value={subject.id} />
-        ))}
-      </Picker>
-      <TouchableOpacity style={styles.saveButton} onPress={handleSave}>
-        <Text style={styles.buttonText}>Salvar</Text>
-      </TouchableOpacity>
-    </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20, marginBottom: 20 }}>
+            <TouchableOpacity style={stopwatchesStyles.historyButton} onPress={handleHistory}>
+              <Text style={stopwatchesStyles.historyButtonText}>Histórico</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={stopwatchesStyles.saveButton} onPress={handleSave}>
+              <Text style={stopwatchesStyles.buttonText}>Salvar</Text>
+            </TouchableOpacity>
+          </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -141,71 +179,5 @@ const formatTime = (time) => {
   const seconds = time % 60;
   return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-    paddingHorizontal: 20,
-  },
-  card: {
-    backgroundColor: '#253494',
-    borderRadius: 10,
-    padding: 20,
-    marginBottom: 20,
-    width: '100%',
-    alignItems: 'center',
-  },
-  timerText: {
-    fontSize: 50,
-    fontWeight: 'bold',
-    color: '#fff',
-    textAlign: 'center',
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
-    width: '100%',
-  },
-  button: {
-    backgroundColor: '#3498db',
-    borderRadius: 25,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 50,
-    height: 50,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 10,
-    padding: 15,
-    marginBottom: 20,
-    width: '80%',
-    backgroundColor: '#f0f0f0',
-  },
-  picker: {
-    width: '80%',
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 10,
-    marginBottom: 20,
-    backgroundColor: '#f0f0f0',
-  },
-  saveButton: {
-    backgroundColor: '#27ae60',
-    paddingVertical: 15,
-    paddingHorizontal: 30,
-    borderRadius: 10,
-  },
-});
 
 export default StopwatchScreen;
